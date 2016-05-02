4254379909abd55c1b00e4bc529248a066b98544
MertCingoz <cingoz@itu.edu.tr>
2016-05-02 21:14:14 +0300

    Score field added for category page

--- WebSites/GradLogin/Category.aspx
+++ WebSites/GradLogin/Category.aspx
@@ -13,22 +13,54 @@
         ConnectionString="<%$ ConnectionStrings:ApplicationServices %>"  
         SelectCommand="SELECT [Score] FROM [Submission] WHERE [Question]=@Question AND [Username]=@Username"></asp:SqlDataSource>
 
+<asp:SqlDataSource ID="ScoreDataSource" runat="server" 
+        ConnectionString="<%$ ConnectionStrings:ApplicationServices %>"  
+        SelectCommand="SELECT a.Score, b.MaxScore FROM (SELECT SUM(Score) AS Score FROM Submission INNER JOIN Map ON 
+                        Map.Question = Submission.Question WHERE (Submission.Username = @Username) AND (Map.Category=@Category)) 
+                        AS a CROSS JOIN (SELECT COUNT(Id) * 100 AS MaxScore FROM Map AS MaxScore WHERE Category=@Category) AS b"></asp:SqlDataSource>
 <body>
 <div id="wrapper">
         <div id="page-wrapper">
             <div class="row">
-                <div class="col-lg-12">
+                <div style="right:5px; position:absolute; overflow:hidden;">
+                        <br />   
+                        <div id="g1" style="height:100px; width:100px;"></div>
+                    </div>
+
+                <div class="col-lg-12" style="width:75%; float:left">
                     <h1 class="page-header">Category: <span><%= Request.QueryString["c"]%></span></h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
+            <br />
             <div id="populate" class="row" runat="server">
             </div>
         </div>
         <!-- /#page-wrapper -->
         </div>
+        <!--gauge-->
+        <script src="js/raphael-2.1.4.min.js"></script>
+        <script src="js/justgage.js"></script>
+
+         <script>
+        <%  ScoreDataSource.SelectParameters.Clear();
+            ScoreDataSource.SelectParameters.Add("Username", Membership.GetUser().UserName);
+            ScoreDataSource.SelectParameters.Add("Category", Request.QueryString["c"]);
+            DataSourceSelectArguments args = new DataSourceSelectArguments();
+            System.Data.DataView dv = (System.Data.DataView)ScoreDataSource.Select(args);          
+        %>
+        var g1 = new JustGage({
+            id: "g1",
+            value: <%=dv.Table.Rows[0]["Score"] %>,
+            min: 0,
+            max: <%=dv.Table.Rows[0]["MaxScore"] %>,
+            title: "<%=Request.QueryString["c"]%>",
+            donut: true,
+            levelColors: ["#ff0000", "#ffff00", "#008000"]
+        });
+        </script>
+
+
         </body>
-
-
 </asp:Content>
--- WebSites/GradLogin/Category.aspx.cs
+++ WebSites/GradLogin/Category.aspx.cs
@@ -29,24 +29,43 @@ public partial class _Category : System.Web.UI.Page
                 SubmissionSource.SelectParameters.Add("Question", row["Question"].ToString());
                 SubmissionSource.SelectParameters.Add("Username", Membership.GetUser().UserName);
                 dvSubmission = (DataView)SubmissionSource.Select(args);
+                string theme = "danger";
                 score = "";
+                int value=0;
                 if (dvSubmission.Table.Rows.Count > 0)
+                {
                     score = "Score: "+dvSubmission.Table.Rows[0]["Score"].ToString()+"/100";
+                    value = Convert.ToInt32(dvSubmission.Table.Rows[0]["Score"]);
+                    if (value == 100)
+                        theme = "success";
+                    else if (value >= 50)
+                        theme = "info";
+                    else
+                        theme = "warning";
+                }
                 populate.InnerHtml +=
                     "<div class=\"col-lg-4\">" +
-                    "<div class=\"panel panel-primary\">" +
-                        "<div class=\"panel-heading\">" +
+                    "<div class=\"panel panel-"+theme+"\">" +
+                        "<div class=\"panel-heading\"><label>" +
                             row["QuestionName"].ToString() +
-                        "</div>" +
+                        "</label></div>" +
                         "<div class=\"panel-body\">" +
-                           "<button onclick=\"location.href=\'Challange.aspx?p=" + row["Question"].ToString() + "'\" type=\"button\" class=\"btn btn-primary\" style=\"float:right;\">Go to Question</button>" +
-                            "<h5 style=\"font-weight:bold\">" + score + "</h5>" +                        
+                           "<div class=\"progress progress-striped active\">"+
+                                "<div class=\"progress-bar progress-bar-" + theme + "\" role=\"progressbar\" aria-valuenow=\"" + value + "\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:" + value + "%\">" +
+                                    "<span class=\"sr-only\"><span>"+score+"</span>% Complete (success)</span>"+
+                                "</div>"+
+                            "</div>"+
+
+                           "<button onclick=\"location.href=\'Challange.aspx?p=" + row["Question"].ToString() + "'\" type=\"button\" class=\"btn btn-" + theme + "\" style=\"float:right;\">Go to Question</button>" +
+                           "<h5 style=\"font-weight:bold\">" + score + "</h5>" +
                            "</div>" +
                         "<div class=\"panel-footer\" style=\"overflow:hidden;\">" +
-                            "<button onclick=\"location.href=\'Leaderboard.aspx?p=" + row["Question"].ToString() + "'\" type=\"button\" class=\"btn btn-outline btn-primary\">Leaderboard</button>" + 
+                            "<button onclick=\"location.href=\'Leaderboard.aspx?p=" + row["Question"].ToString() + "'\" type=\"button\" class=\"btn btn-outline btn-"+theme+"\">Leaderboard</button>" + 
                         "</div>" +
                     "</div>" +
                 "</div>";
+                /*
+                 * */
             }
         }
     }
