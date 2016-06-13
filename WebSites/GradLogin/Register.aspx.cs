46d911000602e8a07fd2f24fb36e36a47227b578
MertCingoz <mertcingoz@gmail.com>
2016-06-13 05:11:00 +0300

    Register issue fixed

--- WebSites/GradLogin/Register.aspx.cs
+++ WebSites/GradLogin/Register.aspx.cs
@@ -17,13 +17,7 @@ public partial class _Register : System.Web.UI.Page
     protected void RegisterUser_CreatedUser(object sender, EventArgs e)
     {
         FormsAuthentication.SetAuthCookie(RegisterUser.UserName, false /* createPersistentCookie */);
-
-        string continueUrl = RegisterUser.ContinueDestinationPageUrl;
-        if (String.IsNullOrEmpty(continueUrl))
-        {
-            continueUrl = "~/";
-        }
-        Response.Redirect(continueUrl);
+        Response.Redirect("Dashboard.aspx");
     }
 
 }
